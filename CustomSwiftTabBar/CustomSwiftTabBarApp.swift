d3f6fed5b793f18b0bb53d628780ae93494ab065
artyyr2336 <67701775+artyyr2336@users.noreply.github.com>
2022-03-20 20:18:55 +0300

    Initial Commit

---
+++ CustomSwiftTabBar/CustomSwiftTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwiftTabBarApp.swift
+//  CustomSwiftTabBar
+//
+//  Created by Артур on 20.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwiftTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
